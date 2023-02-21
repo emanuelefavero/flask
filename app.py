2e42cc4d6380dd1bb84ab65e1b884038a41971e9
emanuelefavero <emafaux89@gmail.com>
2023-02-21 03:26:03 +0100

    add variables and http methods

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return "<h1>Hello, World!</h1>"
+def home():
+    name = "John"
+    # NOTE: variables should be escaped, Jinja2 does this automatically
+    return f"<h1>Hello {name}</h1>"
+
+
+# HTTP METHODS
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # DO THE LOGIN STUFF
+        return "You are now logged in"
+    else:
+        # SHOW THE LOGIN FORM
+        return "Please log in"
